d03f6abadf7973950526a6026cbb1d59fb23e425
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-04-10 19:46:22 +0700

    Update Element.ts

--- src/Element.ts
+++ src/Element.ts
@@ -1,4 +1,5 @@
 interface Element {
+  /** @see {@link https://stackoverflow.com/a/59360710/6404439} */
   insertAfter: (el: HTMLElement) => HTMLElement;
 }
 Element.prototype.insertAfter = function(el) {
